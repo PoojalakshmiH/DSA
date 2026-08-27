cdf6e5775c6e57f3bddb059d399f38c3db090992
Poojalakshmi H <poojalakshmipooja499@gmail.com>
2026-08-27 19:10:20 +0530

    Time: 43 ms (46.09%) | Memory: 115.1 MB (17.31%) - LeetSync

---
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(n);
+
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            adj[u].push_back(v);
+        }
+
+        vector<int>indegree(n);
+
+        for(int i=0;i<n;i++)
+        {
+            for(int v:adj[i])
+            {
+                indegree[v]++;
+            }
+        }
+
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                ans.push_back(i);
+            }
+        }
+        
+
+        return ans;
+    }
+};
